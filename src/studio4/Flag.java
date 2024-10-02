c757e7480b6ea0f385a47bf0fe8fe9535bcf253f
yumi <yumi@localhost>
2024-10-02 15:39:15 -0500

    done

--- src/studio4/Flag.java
+++ src/studio4/Flag.java
@@ -5,7 +5,18 @@ import java.awt.Color;
 import edu.princeton.cs.introcs.StdDraw;
 
 public class Flag {
-	public static void main(String[] args) {
-		
+	public static void main(String[] args) {}
+		private static void mother1() {
+			StdDraw.clear();
+			StdDraw.setPenColor(255, 0, 0);
+			StdDraw.filledRectangle(3, 3, 3, 3);
+			StdDraw.filledRectangle(3, 13, 3, 3);
+			StdDraw.filledRectangle(16, 3, 6, 3);
+			StdDraw.filledRectangle(16, 13, 6, 3);
+			StdDraw.setPenColor(0, 0, 127);
+			StdDraw.filledRectangle(11, 8, 11, 1);
+			StdDraw.filledRectangle(8, 8, 1, 8);
+		}
+	
+
 	}
-}
--- src/studio4/InterpretDrawingFile.java
+++ src/studio4/InterpretDrawingFile.java
@@ -1,5 +1,6 @@
 package studio4;
 
+import java.awt.Color;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
@@ -13,12 +14,34 @@ import edu.princeton.cs.introcs.StdDraw;
  * @author Dennis Cosgrove (http://www.cse.wustl.edu/~cosgroved/)
  */
 public class InterpretDrawingFile {
+	
+	
 
 	public static void main(String[] args) throws FileNotFoundException {
+		
+
+		
+		  StdDraw.setCanvasSize(400, 400);
+	        StdDraw.setXscale(0, 1);
+	        StdDraw.setYscale(0, 1);
+	        StdDraw.setPenColor(Color.CYAN);
+	        
 		JFileChooser chooser = new JFileChooser("resources");
 		chooser.showOpenDialog(null);
 		File f = new File(chooser.getSelectedFile().getPath());
 		Scanner in = new Scanner(f); //making Scanner with a File
+		String c = in.next();
+		int a = in.nextInt();
+		int b = in.nextInt();
+		int d = in.nextInt();
+		boolean halfHeight = in.nextBoolean();
+		double x = in.nextDouble();
+		double y = in.nextDouble();
+		double halfWidth = in.nextDouble();
+		double z = in.nextDouble();
 		
+		
+		
+	
 	}
 }
